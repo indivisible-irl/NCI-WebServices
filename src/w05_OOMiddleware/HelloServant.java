4fb3b79307dbc450d2ba938d3ceac771d98e6a0e
indivisible-irl <mbspare@gmail.com>
2012-10-19 21:51:50 +0100

    end of OOMiddleware tut from wk5 (wk6 now)_2

--- src/w05_OOMiddleware/HelloServant.java
+++ src/w05_OOMiddleware/HelloServant.java
@@ -8,7 +8,7 @@ public class HelloServant extends w05_OOMiddleware.HelloApp._HelloImplBase {
 
 	@Override
 	public String sayHello() {
-		System.out.println("Is it cuz I is black?");
+		System.out.println("Is it cuz I is black? yep");
 		return "\nHello World\n";
 	}
 
